1ed85bf82beafb0ad3a0c5a0e156d20f1bf93533
Osso <a.asamaklil500@GMAIL.COM>
2023-07-15 19:29:51 +0200

    de eeste versie 1.1

--- index.php
+++ index.php
@@ -7,27 +7,58 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Eliso</title>
   <link rel="stylesheet" href="style.css">
-  <script src="js/script.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100&display=swap" rel="stylesheet">
-  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-
-
+  <script src="https://kit.fontawesome.com/f47df9657a.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-<nav>
-  <input type="checkbox" id="check">
-  <label for="check" class="checkbtn">
-    <i class="fas fa-bars"></i>
-  </label>
-  <label class="logo">E L I S O</label>
-  <ul>
-    <li><a class="active" href="#">W O R K S</a></li>
-    <li><a class="active" href="#">A B O U T</a></li>
-    <li><a class="active" href="#">C O N T A C T</a></li>
-  </ul>
-</nav>
+  <div class="card-container">
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+      <label class="logo">E L I S O</label>
+      <ul class="nav-links">
+        <a href="index2.php" class="active">W O R K S</a>
+       <a href="#" class="active">A B O U T</a>
+        <a href="#" class="active">C O N T A C T</a>
+      </ul>
+    </nav>
+    <section class="section">
+      <div class="content">
+        <div class="image-text-wrapper">
+          <img class="img1" src="img/01_v2_Neutral_1.8.2.jpg" alt="">
+          <h1 id="img-text">W H I R L I N G W O R L D S</h1>
+          <a id="goPage" href=""> 
+            GO TO PAGE  <i class="fas fa-arrow-right"></i>
+           </a>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content"> 
+        <div class="image-text-wrapper">
+         <img class="img2" src="img/ELS_SHOOT_HQ_02_23-11.jpg" alt="">
+       <h2 id="img-text2">W H I R L I N G W O R L D S</h2>
+        <a id="goPage2" href="">
+          GO TO PAGE  <i class="fas fa-arrow-right"></i>
+        </a>
+        </div>
+      </div>
+        </section>     
+      </div>
+  
+ 
+    
 
+    <script src="script.js"></script>
 </body>
+
+</html>
--- script.js
+++ script.js
@@ -1,10 +1,105 @@
-const checkbox = document.getElementById('check');
-const ul = document.querySelector('ul');
+  document.addEventListener("DOMContentLoaded", function () {
+      const checkBtn = document.querySelector('.checkbtn');
+      const navLinks = document.querySelector('.nav-links');
 
-checkbox.addEventListener('change', function() {
-  if (this.checked) {
-    ul.classList.add('show');
-  } else {
-    ul.classList.remove('show');
-  }
+      checkBtn.addEventListener('click', () => {
+        checkBtn.classList.toggle('close');
+        navLinks.classList.toggle('show');
+      });
+  
+    // Configureer de ScrollTrigger-plugin
+    gsap.registerPlugin(ScrollTrigger);
+  
+    // Voeg de animatie toe aan de secties
+    const sections = document.querySelectorAll('.section');
+    sections.forEach((section) => {
+      gsap.from(section, {
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        scrollTrigger: {
+          trigger: section,
+          start: 'top center',
+        },
+      });
+    });
+  
+    // Voeg de animatie toe aan de afbeeldingen met class "img1"
+    const img1 = document.querySelector('.img1');
+    gsap.from(img1, {
+      opacity: 0,
+      scale: 0.5,
+      duration: 1,
+      scrollTrigger: {
+        trigger: img1,
+        start: 'top center',
+      },
+    });
+  
+    // Voeg de animatie toe aan de afbeeldingen met class "img2"
+    const img2 = document.querySelectorAll('.img2');
+    img2.forEach((image) => {
+      gsap.from(image, {
+        opacity: 0,
+        scale: 0.5,
+        duration: 1,
+        scrollTrigger: {
+          trigger: image,
+          start: 'top center',
+        },
+      });
+    });
+  
+    // Voeg de Text reveal-animatie toe met een vertraging van 2 seconden
+    const imgText = document.getElementById('img-text');
+    gsap.set(imgText, { opacity: 0, y: '100%' });
+    gsap.to(imgText, {
+      opacity: 1,
+      y: 0,
+      duration: 2.6,
+      ease: 'power4.out',
+      delay: 2,
+    });
+
+  
+    // Voeg de Text reveal-animatie toe met een vertraging van 2 seconden als  "img2" in beeld komt
+    const imgText2 = document.getElementById('img-text2');
+    gsap.set(imgText2, { opacity: 0, y: '100%' });
+    gsap.to(imgText2, {
+      opacity: 1,
+      y: 0,
+      duration: 2.6,
+      ease: 'power4.out',
+      delay: 2,
+      scrollTrigger: {
+        trigger: img2,
+        start: 'top center',
+      },
+    });
+    // voeg de gopage2 animatie toe met een vertraging van 2 seconden als  "img2" in beeld komt
+    const goPage2 = document.getElementById('goPage2');
+    gsap.set(goPage2, { opacity: 0, y: '100%' });
+    gsap.to(goPage2, {
+      opacity: 1,
+      y: 0,
+      duration: 2.6,
+      ease: 'power4.out',
+      delay: 2,
+      scrollTrigger: {
+        trigger: img2,
+        start: 'top center',
+      },
+    });
+      // Voeg de Text reveal-animatie toe met een vertraging van 2 seconden aan het element met id="goPage"
+  const goPage = document.getElementById('goPage');
+  gsap.set(goPage, { opacity: 0, y: '100%' });
+  gsap.to(goPage, {
+    opacity: 1,
+    y: 0,
+    duration: 2.6,
+    ease: 'power4.out',
+    delay: 2,
+  });
 });
+
+  
